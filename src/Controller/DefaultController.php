d0c90a3fecb29a25de44332c3538a94be0eaa07d
Olivier Valette <olivier.valette@spi10.com>
2018-11-22 15:10:37 +0100

    Dynamic dropdown in homepage search

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,8 +23,13 @@ class DefaultController extends BaseController
         $books = $this->getDoctrine()
             ->getRepository(Book::class)
             ->findLast(6);
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findBy([], ['name' => 'ASC']);
+        
         return $this->render('default/homepage.html.twig', [
             "books" => $books,
+            "categories" => $categories,
             ]);
     }
     
