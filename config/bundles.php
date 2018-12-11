6892329492b1064a1a2c4d897abeda429c1dea8d
Olivier Valette <olivier.valette@spi10.com>
2018-12-11 15:33:14 +0100

    Add API authorisations
    using 'composer req cors'

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
