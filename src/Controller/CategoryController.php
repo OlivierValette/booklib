9a98568f45240465cba741ddc4da454bde924d73
Olivier Valette <olivier.valette@spi10.com>
2018-12-12 11:12:23 +0100

    New route 'category' for json API

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -10,8 +12,29 @@ use Symfony\Component\Routing\Annotation\Route;
  * @package App\Controller
  * @Route("/category")
  */
+
 class CategoryController extends BaseController
 {
+    
+    /**
+     * @Route("/", name="category_list", methods="GET")
+     */
+    public function index(Request $request): Response
+    {
+        // request with array output for json API
+        // (to avoid serialization problems)
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->createQueryBuilder('c')
+            ->getQuery()
+            ->getArrayResult();
+        
+        if ($request->isXmlHttpRequest()) {
+            return $this->json($categories);
+        }
+        return new Response('html');
+    }
+    
     /**
      * @Route("/show/{id}", name="category_show")
      */
